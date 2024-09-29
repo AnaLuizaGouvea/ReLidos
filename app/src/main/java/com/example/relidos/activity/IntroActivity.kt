a088212d5602d78353e0c7b12682f0926f2d9d2f
Kamilly Gomes <157547650+kamscristine@users.noreply.github.com>
2024-09-28 23:02:18 -0300

    Commitados: Dashboard Adapters UI, Banners in Dashboard e Official Brand in Dashboard

--- app/src/main/java/com/example/relidos/activity/IntroActivity.kt
+++ app/src/main/java/com/example/relidos/activity/IntroActivity.kt
@@ -2,12 +2,6 @@ package com.example.relidos.activity
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.relidos.MainActivity
-import com.example.relidos.R
 import com.example.relidos.databinding.ActivityIntroBinding
 
 class IntroActivity : BaseActivity() {
